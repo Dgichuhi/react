7af12289ba2c67d80d332f62174e70d35dba73eb
Dominic Gichuhi <gichuhimd@gmail.com>
2017-02-21 16:32:47 +0300

    added reset.css and basic styling start

---
+++ react/public/stylesheets/style.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  'import': ''reset.css'',
+  'html body': {
+    'background': '#fbfbfb',
+    'color': 'rgba(0, 0, 0, .8)',
+    'fontFamily': '20px 'Open Sans', Helvetica, Arial, sans-serif'
+  },
+  'body': {
+    'padding': [{ 'unit': 'px', 'value': 50 }, { 'unit': 'px', 'value': 50 }, { 'unit': 'px', 'value': 50 }, { 'unit': 'px', 'value': 50 }],
+    'font': [{ 'unit': 'px', 'value': 14 }, { 'unit': 'string', 'value': '"Lucida' }, { 'unit': 'string', 'value': 'Grande",' }, { 'unit': 'string', 'value': 'Helvetica,' }, { 'unit': 'string', 'value': 'Arial,' }, { 'unit': 'string', 'value': 'sans-serif' }]
+  },
+  'a': {
+    'color': '#00B7FF'
+  }
+});
